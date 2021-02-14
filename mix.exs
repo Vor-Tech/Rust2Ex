aa32a0d3fc5f55887ed328330dbfed4d1896b5de
Roland Metivier <metivier.roland@chlorophyt.us>
2021-02-14 15:17:42 -0600

    v0.1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rust2ex.MixProject do
   def project do
     [
       app: :rust2ex,
-      version: "0.1.3",
+      version: "0.1.4",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
